21972f70a4df306081ab29507e1654238cd31364
tami-cp0 <findtamilore@gmail.com>
2025-04-19 12:09:59 +0000

    add function guard and prevent server-side injection

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -27,18 +27,20 @@ function ContributionsHandler(db) {
 
     this.handleContributionsUpdate = (req, res, next) => {
 
-        /*jslint evil: true */
-        // Insecure use of eval() to parse inputs
-        const preTax = eval(req.body.preTax);
-        const afterTax = eval(req.body.afterTax);
-        const roth = eval(req.body.roth);
+        // Validate if inputs exist
+        if (!req.body.preTax || !req.body.afterTax || !req.body.roth) {
+            return res.render("contributions", {
+                updateError: "All contribution percentages are required",
+                userId: req.session.userId,
+                environmentalScripts
+            });
+        }
 
-        /*
-        //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
-        const preTax = parseInt(req.body.preTax);
-        const afterTax = parseInt(req.body.afterTax);
-        const roth = parseInt(req.body.roth);
-        */
+        // Convert inputs to numbers
+        const preTax = parseInt(req.body.preTax, 10);
+        const afterTax = parseInt(req.body.afterTax, 10);
+        const roth = parseInt(req.body.roth, 10);
+
         const {
             userId
         } = req.session;
